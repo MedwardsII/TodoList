ae6f67d283b343e5008e76ee442469c5f8d03ddf
Michael N. Edwards II <mnedwards118@gmail.com>
2022-01-04 22:18:53 -0800

    created Procfile

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from tasks import views
 
 app_name = 'tasks'
 urlpatterns = [
-    path('tasks', views.task_create, name='task_list'),
+    path('', views.task_create, name='task_list'),
     path('tasks/edit/<int:pk>', views.task_edit, name='task_edit'),
     path('tasks/delete/<int:pk>', views.task_delete, name='task_delete'),
     path('tasks/update-status/<int:pk>', views.task_update_stats, name='task_update_status')
--- todolist/settings.py
+++ todolist/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Login redirection
-LOGIN_REDIRECT_URL = '/tasks'
+LOGIN_REDIRECT_URL = '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
