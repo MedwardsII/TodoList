faed9c9d221f4a5b2c7e9d6fa382df26cf60ed91
Michael N. Edwards II <mnedwards118@gmail.com>
2022-01-10 22:39:58 -0800

    updated database connections

--- todolist/settings.py
+++ todolist/settings.py
@@ -132,4 +132,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 django_heroku.settings(locals())
